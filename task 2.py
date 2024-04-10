45f6f905bbaa1bcab1c6a9d79a6d51ef8eee8656
R0lt1k <153744562+R0lt1k@users.noreply.github.com>
2024-04-10 18:07:16 +0500

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+def load_urls(url, file_name):
+    response = requests.get(url)
+    with open(file_name, "w") as file:
+        file.write(response.text)
+
+start = time.time()
+for i in range(1, 10):
+    load_urls(url, f"post{i}.json")
+end = time.time()
+print("Время выполнения: ", end - start)
